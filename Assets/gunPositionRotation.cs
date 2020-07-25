a2f58aa19d85deab78e9941ec90be913c1674751
Avi Chapman <avichapman@gmail.com>
2020-07-25 15:18:32 +0930

    Update gunPositionRotation.cs
    Updated rotation of gun.

--- Assets/gunPositionRotation.cs
+++ Assets/gunPositionRotation.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,13 +15,16 @@ public class gunPositionRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = player.rotation;
         transform.position = player.position;
-        transform.position = new Vector3(player.position.x + 1, player.position.y, player.position.z);
-        transform.rotation = new Quaternion(0,90,90,0);
-        //        transform.rotation = player.rotation;
-        //transform.position += transform.forward * 0.827f;
-        //transform.position += -transform.right * -0.296f;
-        //transform.position += transform.up * 0.142f;
-        //transform.rotation = new Quaternion(camera.rotation.x + 0.75f, player.rotation.y + 0.25f, camera.rotation.z, 1);
+        transform.rotation = transform.rotation * Quaternion.Euler(camera.rotation.eulerAngles.x, 0, 0);
+        transform.position += transform.forward * 0.827f;
+        transform.position += -transform.right * -0.296f;
+        transform.position += transform.up * 0.142f;
+        transform.rotation = transform.rotation * Quaternion.Euler(-90, 90, 0);
     }
 }
